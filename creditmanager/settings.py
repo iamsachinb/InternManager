0cff73be2313ac1e34ca96d202229244de44b87e
John Thomas <johnthomasajs@gmail.com>
2024-01-23 15:23:06 +0530

    Update settings.py

--- creditmanager/settings.py
+++ creditmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pzu=g6m1ew-(lr(z7yrvses$mlzlpfc^+(v&ni6#t!1#hx_5zw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://intern-manager-86sw.onrender.com/']
 
 
 # Application definition
