6c5f6fefaa4ab52882fbfa74b897c34473340b73
johntheajs <johnthomasajs@gmail.com>
2024-02-02 10:58:09 +0530

    Final-1

--- creditmanager/settings.py
+++ creditmanager/settings.py
@@ -156,7 +156,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/files')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
