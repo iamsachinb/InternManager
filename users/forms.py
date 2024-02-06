6bfdd265fc09af613b022008545a12953464c288
johntheajs <johnthomasajs@gmail.com>
2024-02-07 01:00:39 +0530

    instructions-3

--- users/forms.py
+++ users/forms.py
@@ -39,14 +39,17 @@ class ProfileForm(ModelForm):
 class InternForm(ModelForm):
     class Meta:
         model = Intern
-        fields = ['name', 'permission',  'certificate', 'report']
+        fields = ['name', 'permission', 'certificate', 'report']
 
     def __init__(self, *args, **kwargs):
         super(InternForm, self).__init__(*args, **kwargs)
 
-        # self.fields['title'].widget.attrs.update({'class':'input', 'placeholder':'Add title'})
-        # self.fields['title'].widget.attrs.update({'class':'input', 'placeholder':'Add title'})
-
-        for name,field in self.fields.items():
-            field.widget.attrs.update({'class':'input', 'required': 'true'})
+        # Update widget attributes for all fields
+        for name, field in self.fields.items():
+            # Set required attribute to True for all fields except 'permission'
+            if name != 'permission':
+                field.widget.attrs.update({'class': 'input', 'required': 'true'})
+            else:
+                # For 'permission' field, do not set required attribute
+                field.widget.attrs.update({'class': 'input'})
 
