0c9668e78f4ed93c348e9d0a4266595ccc23766d
johntheajs <johnthomasajs@gmail.com>
2024-03-10 11:39:41 +0530

    app password

--- creditmanager/settings.py
+++ creditmanager/settings.py
@@ -137,7 +137,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'tceitintern@gmail.com'
-EMAIL_HOST_PASSWORD = 'lzllchsaycesnwma'
+EMAIL_HOST_PASSWORD = 'qrkszssoclrqxauv'
 
 
 # Static files (CSS, JavaScript, Images)
